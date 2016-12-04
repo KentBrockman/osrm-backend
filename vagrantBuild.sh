03dfa472cc4c13fe435b533e125d2a3e2c62a490
Kent Brockman <brockman.ks@gmail.com>
2016-12-03 19:27:22 -0700

    I am a noob

--- vagrantBuild.sh
+++ vagrantBuild.sh
@@ -1,4 +1,5 @@
 # Install dependencies for building on 16.04 as per https://github.com/Project-OSRM/osrm-backend/wiki/Building-on-Ubuntu
+sudo apt-get update
 sudo apt install build-essential git cmake pkg-config \
 libbz2-dev libstxxl-dev libstxxl1v5 libxml2-dev \
 libzip-dev libboost-all-dev lua5.2 liblua5.2-dev libluabind-dev libtbb-dev -y
